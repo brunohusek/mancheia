19369431118f14bdb43ebe35b855735681dac104
Bruno Husek <brunohusek@gmail.com>
2017-06-18 17:57:06 -0300

    Composer

--- public/index.php
+++ public/index.php
@@ -1,23 +1,5 @@
-<!DOCTYPE html>
-<html ng-app lang="pt-br">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="/css/bootstrap.min.css" rel="stylesheet">
-    <script src="/js/angular.min.js"></script>
-    <script src="/js/jquery-1.12.4.min.js"></script>
-    <script src="/js/bootstrap.min.js"></script>
-    <title>Mancheia</title>
-  </head>
-  <body>
-    <h1>Olá, mundo!</h1>
+<?php
 
-    <div>
-      <label>Name:</label>
-      <input type="text" ng-model="yourName" placeholder="Enter a name here">
-      <hr>
-      <h1>Hello {{yourName}}!</h1>
-    </div>
-  </body>
-</html>
+require_once "../vendor/autoload.php";
+
+$teste = new Mancheia\ClassesDeTestes\Testando();
--- src/Mancheia/classes.php
+++ src/Mancheia/classes.php
@@ -1,2 +1,9 @@
 <?php
-?>
+
+namespace Mancheia\ClassesDeTestes;
+
+class Testando {
+  public function __construct() {
+    echo "Lendo a classe";
+  }
+}
