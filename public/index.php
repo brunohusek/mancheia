c7dcc3ad0248931da9c8f2791ae59ee2d5306707
Bruno Husek <brunohusek@gmail.com>
2017-06-18 21:18:11 -0300

    Ajustes

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
 
-require_once "../vendor/autoload.php";
+//require_once "../vendor/autoload.php";
+require_once "../src/Mancheia/Classes.php";
 
 $teste = new Mancheia\ClassesDeTestes\Testando();
